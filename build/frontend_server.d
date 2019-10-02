fb8de189407b4e30e60b8aa5406d9e0423d2ffd0
Oscar Heimdahl <o.heimdahl@gmail.com>
2019-10-02 12:15:14 +0200

    Git ignore added maybe?

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /home/albin/development/flutter/bin/cache/artifacts/engine/linux-x64/frontend_server.dart.snapshot
+frontend_server.d: /Library/Flutter/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
