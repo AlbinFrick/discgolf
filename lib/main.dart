f572fb84eed31d622b4b5dac226e42a1153cd76a
Oscar Heimdahl <o.heimdahl@gmail.com>
2019-10-03 16:19:55 +0200

    Added firestore for android.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
       home: Home(),
     );
   }
--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+        child: Center(
+          child: StreamBuilder(
+            stream: Firestore.instance.collection('courses').snapshots(),
+            builder: (context, snap) {
+              if (snap.hasData) {
+                print(snap.data.documents[0]['address']);
+                return Text(snap.data.documents[0]['address'].toString());
+              }
+              return Text('noData');
+            },
+          ),
+        ),
+      ),
     );
   }
 }
