de34af78adf8e49ea1bb16defd367fcba7640e8d
Oscar Heimdahl <o.heimdahl@gmail.com>
2019-10-03 13:45:18 +0200

    Added some initial navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:discgolf/screens/user.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -19,16 +20,65 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Widget> tabs = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    )
+  ];
+  int bodyIndex = 0;
+
+  changeHomeIndex(int index) {
+    setState(() {
+      bodyIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            height: 20,
-          ),
+        bottomNavigationBar: Row(
+          children: <Widget>[
+            TabButton(
+              onPress: changeHomeIndex,
+              index: 0,
+            ),
+            TabButton(
+              onPress: changeHomeIndex,
+              index: 1,
+            )
+          ],
         ),
-        body: Container(
-          color: Colors.red,
+        body: Stack(
+          children: <Widget>[
+            tabs[bodyIndex],
+            Center(
+              child: RaisedButton(onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => User()),
+                );
+              }),
+            ),
+          ],
+        ));
+  }
+}
+
+class TabButton extends StatelessWidget {
+  final Function onPress;
+  final int index;
+  TabButton({this.onPress, this.index});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => onPress(index),
+        child: Container(
+          width: 50,
+          height: 50,
+          child: Icon(Icons.access_alarm),
         ));
   }
 }
