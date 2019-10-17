b17ccb5d8ae07e580067d169913ce744f36b489c
oscarheimdahl <o.heimdahl@gmail.com>
2019-10-17 20:38:09 +0200

    added style to invite friends screen.

--- lib/screens/invite_friends.dart
+++ lib/screens/invite_friends.dart
@@ -1,15 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:discgolf/screens/user.dart';
 import 'package:discgolf/utils/colors.dart';
+import 'package:discgolf/widgets/list_title.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class InviteFriends extends StatelessWidget {
+  DocumentSnapshot userSnapshot;
   getPlayerFriends(String uid) async {
-    DocumentSnapshot userSnapshot =
+    userSnapshot =
         await Firestore.instance.collection('users').document(uid).get();
-    List<String> friendIDs = List<String>.from(userSnapshot.data['friends']);s
+    List<String> friendIDs = List<String>.from(userSnapshot.data['friends']);
     List<Map<String, dynamic>> friends = List();
     friends = await getFriends(friendIDs, friends);
     print('friends done loading');
@@ -22,7 +24,9 @@ class InviteFriends extends StatelessWidget {
           .collection('users')
           .document(friendIDs[i])
           .get();
-      if (user != null && user.data != null) friends.add(user.data);
+      user.data['index'] = i;
+      // if (user != null && user.data != null)
+      friends.add(user.data);
     }
     return friends;
   }
@@ -41,7 +45,7 @@ class InviteFriends extends StatelessWidget {
           future: getPlayerFriends(uid),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done)
-              return FriendAdder(friends: snapshot.data);
+              return FriendAdder(friends: snapshot.data, user: userSnapshot);
             return Container(
                 // color: Colors.black,
                 );
@@ -52,7 +56,8 @@ class InviteFriends extends StatelessWidget {
 
 class FriendAdder extends StatefulWidget {
   final List friends;
-  FriendAdder({this.friends});
+  final DocumentSnapshot user;
+  FriendAdder({this.friends, this.user});
 
   @override
   _FriendAdderState createState() => _FriendAdderState();
@@ -60,23 +65,45 @@ class FriendAdder extends StatefulWidget {
 
 class _FriendAdderState extends State<FriendAdder> {
   List addedPlayers = List();
+  var user;
+
+  @override
+  void initState() {
+    super.initState();
+    // loadUser();
+    addedPlayers.add(widget.user);
+  }
+
+  // loadUser(uid) async {
+  //   DocumentSnapshot userSnapshot =
+  //       await Firestore.instance.collection('users').document(uid).get();
+  //   user = userSnapshot.data;
+  //   setState(() {
+  //     addedPlayers.add(user);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
+    // final String uid = Provider.of<FirebaseUser>(context).uid;
+    // if (user == null) loadUser(uid);
     return Container(
+      padding: EdgeInsets.fromLTRB(5, 5, 5, 0),
       child: Column(
         children: <Widget>[
+          SizedBox(height: 5),
+          ListTitle('Spelare'),
+          PlayersList(
+              players: addedPlayers,
+              onRemove: (player, e) {
+                setState(() {
+                  widget.friends.insert(player['index'], player);
+                  addedPlayers.remove(player);
+                });
+              }),
+          ListTitle('Vänner'),
           Flexible(
-            child: PlayersList(
-                players: addedPlayers,
-                onRemove: (player, e) {
-                  setState(() {
-                    widget.friends.add(player);
-                    addedPlayers.remove(player);
-                  });
-                }),
-          ),
-          Flexible(
+            flex: 2,
             child: FriendList(
                 friends: widget.friends,
                 onAdd: (player) {
@@ -99,14 +126,17 @@ class PlayersList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         child: ListView.builder(
+      shrinkWrap: true,
       itemCount: players.length,
       itemBuilder: (context, index) {
         return FriendCard(
+          dismissable: true,
           friend: players[index],
           onAdd: (a, b) {
             onRemove(players[index], 'a');
           },
         );
+        // );
       },
     ));
   }
@@ -120,19 +150,21 @@ class FriendList extends StatelessWidget {
 
   onAddList(player, index) {
     onAdd(player);
-    friends.removeAt(index);
+    //removes the friend, but keeps the length of the list.
+    friends[index] = null;
   }
 
   @override
   Widget build(BuildContext context) {
-    // final String uid = Provider.of<FirebaseUser>(context).uid;
-
     return Container(
         child: ListView.builder(
       itemCount: friends.length,
       itemBuilder: (context, index) {
-        return FriendCard(
-            friend: friends[index], onAdd: onAddList, index: index);
+        var friend = friends[index];
+        if (friend != null)
+          return FriendCard(
+              friend: friends[index], onAdd: onAddList, index: index);
+        return Container();
       },
     ));
   }
@@ -141,16 +173,50 @@ class FriendList extends StatelessWidget {
 class FriendCard extends StatelessWidget {
   final Function onAdd;
   final friend;
+  final bool dismissable;
   final int index;
-  FriendCard({@required this.friend, @required this.onAdd, this.index});
+  FriendCard(
+      {@required this.friend,
+      @required this.onAdd,
+      this.index,
+      this.dismissable = false});
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => onAdd(friend, index),
-      child: Card(
-        color: Colors.red,
-        child: ListTile(leading: Text(friend['email'])),
-      ),
+    Card friendCard = Card(
+      elevation: 4,
+      color: mainColor,
+      margin: EdgeInsets.all(dismissable ? 0 : 4),
+      child: ListTile(
+          title: Text(
+        friend['email'],
+        style: TextStyle(
+            fontSize: 15, color: accentColor, fontWeight: FontWeight.bold),
+      )),
     );
+    return dismissable
+        ? Padding(
+            padding: EdgeInsets.all(4),
+            child: Dismissible(
+              direction: DismissDirection.endToStart,
+              key: Key(friend['index'].toString()),
+              child: friendCard,
+              onDismissed: (dir) {
+                onAdd(friend, index);
+              },
+              background: Card(
+                color: Colors.red[800],
+                child: Align(
+                    alignment: Alignment(0.95, 0),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    )),
+              ),
+            ))
+        : GestureDetector(
+            onTap: () {
+              onAdd(friend, index);
+            },
+            child: friendCard);
   }
 }
