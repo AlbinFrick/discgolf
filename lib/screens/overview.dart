3ed191ef8f5f0f7b63ba860f9904fd33f1ceb2f5
jonathansmedborn <jonathan.smedborn@gmail.com>
2019-10-26 00:43:49 +0200

    Översikt fix

--- lib/screens/overview.dart
+++ lib/screens/overview.dart
@@ -8,55 +8,88 @@ class Overview extends StatelessWidget {
     final Map args = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-        body: SingleChildScrollView(
-      child: FutureBuilder(
-        future: Firestore.instance
-            .collection('games')
-            .document(args['gameid'])
-            .get(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data.exists) {
-            List<DataColumn> dataColumns = List<DataColumn>();
-            DataColumn holes = DataColumn(label: Text("Hål"));
-            dataColumns.add(holes);
-            args['players'].forEach((player) {
-              DataColumn playerCol = DataColumn(label: Text(player['firstname']));
-              dataColumns.add(playerCol);
-            });
+        body: SafeArea(
+      child: Container(
+        color: Colors.grey[700],
+        child: SingleChildScrollView(
+          child: FutureBuilder(
+            future: Firestore.instance
+                .collection('games')
+                .document(args['gameid'])
+                .get(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data.exists) {
+                List<DataColumn> dataColumns = List<DataColumn>();
+                DataColumn holes = DataColumn(
+                    label: Center(
+                  child: Text(
+                    "Hål",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ));
+                dataColumns.add(holes);
+                args['players'].forEach((player) {
+                  DataColumn playerCol = DataColumn(
+                      label: Center(
+                    child: Text(
+                      player['firstname'],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ));
+                  dataColumns.add(playerCol);
+                });
 
-            List<DataRow> dataRows = List<DataRow>();
-            int length;
-            snapshot.data['players'].forEach((id, player){
-            length = snapshot.data['players'][id]['holes'].length;  
-            });
+                List<DataRow> dataRows = List<DataRow>();
+                int length;
+                snapshot.data['players'].forEach((id, player) {
+                  length = snapshot.data['players'][id]['holes'].length;
+                });
 
-            for(int i = 1; i <= length; i++ ){
-              List<DataCell> cells = List<DataCell>();
-              cells.add(DataCell(Text(i.toString()), placeholder: true));
-              
-               snapshot.data['players'].forEach((id, player){
-                 print(player['holes']);
-                 cells.add(DataCell(Text(player['holes'][i.toString()]['throws'].toString()), placeholder: true));
-                 
-            });
-            dataRows.add(DataRow(cells: cells));
-            }
+                for (int i = 1; i <= length; i++) {
+                  List<DataCell> cells = List<DataCell>();
+                  cells.add(DataCell(
+                      Center(
+                          child: Text(
+                        i.toString(),
+                        style: TextStyle(color: Colors.white),
+                      )),
+                      placeholder: true));
 
-            
+                  snapshot.data['players'].forEach((id, player) {
+                    print(player['holes']);
+                    cells.add(DataCell(
+                        Center(
+                          child: Text(
+                            player['holes'][i.toString()]['throws'].toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        placeholder: true));
+                  });
+                  dataRows.add(DataRow(cells: cells));
+                }
 
-            return DataTable(
-              columns: dataColumns,
-              rows: dataRows,
-            );
-          }
-          return Container(
-            child: Center(
-              child: CupertinoActivityIndicator(
-                radius: 20,
-              ),
-            ),
-          );
-        },
+                return DataTable(
+                  columns: dataColumns,
+                  rows: dataRows,
+                );
+              }
+              return Container(
+                child: Center(
+                  child: CupertinoActivityIndicator(
+                    radius: 20,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ),
     ));
   }
